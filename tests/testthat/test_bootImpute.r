40985940f5ce9eae5bc3fe0a40d92fba45e1b02a
Jonathan Bartlett <jonathan.bartlett1@lshtm.ac.uk>
2025-03-24 22:03:40 +0000

    Updating corresponding test that uses ex_linquad

--- tests/testthat/test_bootImpute.r
+++ tests/testthat/test_bootImpute.r
@@ -162,8 +162,8 @@ test_that("Testing bootSmcfcs works", {
       #bootstrap twice and impute each twice
       #in practice you should bootstrap many more times, e.g. at least 200
       imps <- bootSmcfcs(ex_linquad, nBoot=2, nImp=2,
-                         smtype="lm", smformula="y~z+x+xsq",
-                         method=c("","","norm","x^2",""))
+                         smtype="lm", smformula="y~z+x+I(x^2)",
+                         method=c("","","norm",""))
     }
   }, "It is recommended to use at least 200 bootstraps.")
 })
