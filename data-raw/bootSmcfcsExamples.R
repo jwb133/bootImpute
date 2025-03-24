31881945d54ad6ead0517bfff42a1667f9e06aa5
Jonathan Bartlett <jonathan.bartlett1@lshtm.ac.uk>
2025-03-24 22:01:46 +0000

    Updating smcfcs example following change to ex_linquad in smcfcs.

--- data-raw/bootSmcfcsExamples.R
+++ data-raw/bootSmcfcsExamples.R
@@ -4,5 +4,5 @@ head(ex_linquad)
 #bootstrap twice and impute each twice
 #in practice you should bootstrap many more times, e.g. at least 200
 imps <- bootSmcfcs(ex_linquad, nBoot=2, nImp=2,
-                   smtype="lm", smformula="y~z+x+xsq",
-                   method=c("","","norm","x^2",""), seed=564764)
+                   smtype="lm", smformula="y~z+x+I(x^2)",
+                   method=c("","","norm",""), seed=564764)
